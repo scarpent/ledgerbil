44da098b22ce8058d883c52f7883ca45aabf9256
Scott Carpenter <scarpent@nerdery.com>
2013-05-25 20:24:25 -0500

    move schedule thing config logic from schedulefile to schedulething

--- schedulefile.py
+++ schedulefile.py
@@ -16,19 +16,6 @@ from schedulething import ScheduleThing
 
 class ScheduleFile(LedgerFile):
 
-    daysAheadRegex = r'''(?x)               # verbose mode
-        ^                                   # line start
-        \s*;;\s*scheduler\s*                # required
-        (?:                                 # non-capturing
-            ;\s*enter\s+(\d+)\s+days?\s*    # days ahead to enter trans
-        )?                                  # optional
-        (?:                                 # non-capturing
-            ;\s*preview\s+(\d+)\s+days?\s*  # days to "preview" trans
-        )?                                  # optional
-        (?:\s*;\s*)?                        # optional ending semi-colon
-        $                                   # line end
-        '''
-
     def __init__(self, filename):
         self.enterDays = -1
         self.previewDays = -1
@@ -36,7 +23,7 @@ class ScheduleFile(LedgerFile):
         super(ScheduleFile, self).__init__(filename)
 
         print('\nSchedule file (enter days = %s, preview days = %s):\n'
-              % (self.enterDays, self.previewDays))
+              % (ScheduleThing.enterDays, ScheduleThing.previewDays))
         self.printFile()
 
     def _addThingLines(self, lines):
@@ -44,32 +31,3 @@ class ScheduleFile(LedgerFile):
             thing = ScheduleThing(lines)
             self.addThing(thing)
 
-    def addThing(self, thing):
-        super(ScheduleFile, self).addThing(thing)
-        if thing.thingNumber == 0:
-            self.handleFirstThing()
-        else:
-            self.handleSubsequentThings(thing.thingNumber)
-
-    # start out by requiring that the ";; scheduler" line is first in file
-    def handleFirstThing(self):
-        ENTER_DAYS = 1
-        PREVIEW_DAYS = 2
-        firstline = self.getThings()[0].getLines()[0]
-
-        match = re.match(self.daysAheadRegex, firstline)
-        if match:
-            if match.group(ENTER_DAYS):
-                self.enterDays = match.group(ENTER_DAYS)
-            if match.group(PREVIEW_DAYS):
-                self.previewDays = match.group(PREVIEW_DAYS)
-        else:
-            raise Exception(
-                '%s is not a valid schedule file - \n'
-                'first line must have scheduler directive, e.g.:\n'
-                ';; scheduler ; enter 7 days ; preview 30 days'
-                % self.filename
-            )
-
-    def handleSubsequentThings(self, thingNumber):
-        print ('this is thing %s' % (self.getThings()[thingNumber].thingNumber + 1))
--- schedulething.py
+++ schedulething.py
@@ -9,12 +9,66 @@ __license__ = 'gpl v3 or greater'
 __email__ = 'scottc@movingtofreedom.org'
 
 
+import re
 
 from ledgerthing import LedgerThing
 
 
 class ScheduleThing(LedgerThing):
 
+    firstThing = True
+    enterDays = -1
+    previewDays = -1
+
+    # individual schedule items (second line of thing)
+    scheduleThingRegex = r'\s*;;\s*schedule\s*'
+
     def __init__(self, lines):
+        self.isAScheduleThing = False
+
         super(ScheduleThing, self).__init__(lines)
 
+        if ScheduleThing.firstThing:
+            self.handleFileConfig(lines[0])
+            ScheduleThing.firstThing = False
+
+        # todo: test single line thing, although would be invalid thing
+        match = re.match(ScheduleThing.scheduleThingRegex, lines[1])
+        if match:
+            self.handleThingConfig(lines[1])
+
+    def handleFileConfig(self, line):
+
+        configRegex = r'''(?x)                  # verbose mode
+            ^                                   # line start
+            \s*;;\s*scheduler\s*                # required
+            (?:                                 # non-capturing
+                ;\s*enter\s+(\d+)\s+days?\s*    # days ahead to enter trans
+            )?                                  # optional
+            (?:                                 # non-capturing
+                ;\s*preview\s+(\d+)\s+days?\s*  # days to "preview" trans
+            )?                                  # optional
+            (?:\s*;\s*)?                        # optional ending semi-colon
+            $                                   # line end
+            '''
+
+        ENTER_DAYS = 1
+        PREVIEW_DAYS = 2
+
+        match = re.match(configRegex, line)
+        if not match:
+            raise Exception(
+                'Invalid schedule file. First line must have '
+                'a scheduler directive. For example:\n'
+                ';; scheduler ; enter 7 days ; preview 30 days'
+            )
+
+        ScheduleThing.isValidScheduleFile = True
+        if match.group(ENTER_DAYS):
+            ScheduleThing.enterDays = match.group(ENTER_DAYS)
+        if match.group(PREVIEW_DAYS):
+            ScheduleThing.previewDays = match.group(PREVIEW_DAYS)
+
+    def handleThingConfig(self, line):
+        self.isAScheduleThing = True
+        pass
