4a10b226bac32dca57743049a213815444439320
Scott Carpenter <scottc@movingtofreedom.org>
2017-12-26 17:18:40 -0600

    Add sample data

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from ledgershell import investments, prices
 argv = sys.argv[1:]
 
 if argv:
+
     if argv[0] == 'inv' or argv[0] == 'investments':
         investments.main(argv[1:])
+
     elif argv[0] == 'prices':
         prices.main(argv[1:])
+
     else:
         ledgerbil.main(argv)
+
 else:
     ledgerbil.main(argv)
